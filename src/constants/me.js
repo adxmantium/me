15d9ec7a5663c4bb1f794eae71eb47fc7b29ab6a
Adam Adams <ajadams10@gmail.com>
2018-02-19 13:31:53 -0800

    adding commas to each object key value except the last one

--- src/constants/me.js
+++ src/constants/me.js
@@ -8,16 +8,16 @@ const SOCIAL = {
 	gh: {name: 'Github', link: 'https://github.com/ajadams', icon: <i className='fab fa-github' />, text: '"Check out my latest code here."', hoverText: 'I currently have 3 pending side-projects that are not live yet.'},
 	ig: {name: 'Instagram', link: 'https://www.instagram.com/adam.squared/', icon: <i className='fab fa-instagram' />, text: '"This is where I like to share new things that I am doing."', hoverText: 'Follow me here to see all my latest work related to my hobbies.'},
 	fb: {name: 'Facebook', link: 'https://www.facebook.com/adam.adams.372', icon: <i className='fab fa-facebook' />, text: '"Another place where you may contact me."', hoverText: 'I check FB very rarely, but feel free to message me on Messenger.'},
-	me: {name: 'Medium', link: 'https://medium.com/@ajadams10', icon: <i className='fab fa-medium' />, text: '"I like to write about my interests here."', hoverText: 'I currently have 4,000+ views on all of my Medium articles.'},
+	me: {name: 'Medium', link: 'https://medium.com/@ajadams10', icon: <i className='fab fa-medium' />, text: '"I like to write about my interests here."', hoverText: 'I currently have 4,000+ views on all of my Medium articles.', noComma: true},
 	un: {name: 'Unsplash', link: 'https://unsplash.com/@atom7dev', icon: <i className='fas fa-camera' />, text: '"Sharing photos I take for others to freely use here."', hoverText: 'I currently have 14,000+ views on all of my Unsplash photos.'},
 	in: {name: 'Linkedin', link: 'https://www.linkedin.com/in/adamadams10/', icon: <i className='fab fa-linkedin' />, text: '"Check out out my professional experience here."', hoverText: 'I am currently open to new opportunities!'},
 	em: {name: 'Email', link: 'mailto:ajadams10@gmail.com', icon: <i className='fas fa-envelope' />, text: '"Best way to contact me!"', hoverText: 'ajadams@gmail.com'},
-	lo: {name: 'Location', link: 'https://www.google.com/maps/place/Walnut+Creek,+CA/@37.8942235,-122.0402229,13z/data=!3m1!4b1!4m5!3m4!1s0x808ff474197ba5f3:0xe06e49972003c36c!8m2!3d37.9100783!4d-122.0651819?hl=en', icon: <i className='fas fa-compass' />, text: '"Walnut Creek, CA"', hoverText: 'Walnut Creek, CA'},
+	lo: {name: 'Location', link: 'https://www.google.com/maps/place/Walnut+Creek,+CA/@37.8942235,-122.0402229,13z/data=!3m1!4b1!4m5!3m4!1s0x808ff474197ba5f3:0xe06e49972003c36c!8m2!3d37.9100783!4d-122.0651819?hl=en', icon: <i className='fas fa-compass' />, text: '"Walnut Creek, CA"', hoverText: 'Walnut Creek, CA', noComma: true},
 }
 
-const socialCode = ({ name, link, icon, text, hoverText }) => (
+const socialCode = ({ name, link, icon, text, hoverText, noComma }) => (
 	<div className="social">
-		<div className="name show">{ name }: { text }</div>
+		<div className="name show">{ name }: { text }{!noComma && ','}</div>
 		<div className="icon showOnHover">
 			<a href={link} target="_blank">{icon}</a> <span className="social-descrip">{hoverText || text}</span>
 		</div>
@@ -129,21 +129,21 @@ const skillsComponents = [
 	},
 		{
 			code: codeExplain({
-				code: "HTML: 8/10",
+				code: "HTML: 8/10,",
 				explain: "HTML: 8/10"
 			}),
 			codeClass: 'objProp'
 		},
 		{
 			code: codeExplain({
-				code: "CSS: 8/10",
+				code: "CSS: 8/10,",
 				explain: "I always appreciate when I am greeted with a mobile-friendly webpage, so I have adopted the mobile-first design strategy. I have used frameworks like Bootstrap and Zurb Foundation, but I prefer to write my own CSS."
 			}),
 			codeClass: 'objProp'
 		},
 		{
 			code: codeExplain({
-				code: "JS: 8.5/10",
+				code: "JS: 8.5/10,",
 				explain: "I have experience with various JS libraries/frameworks/tools including React, React Native, Redux, NodeJS, and Socketio to name a few."
 			}),
 			codeClass: 'objProp'
@@ -183,14 +183,14 @@ const projectComponents = [
 		},
 			{
 				code: codeExplain({
-					code: 'name: "SnowboardSizer"',
+					code: 'name: "SnowboardSizer",',
 					explain: 'This app helps beginner snowboard shoppers find the right snowboard size for them.'
 				}),
 				codeClass: 'objProp2'
 			},
 			{
 				code: codeExplain({
-					code: 'toolsList: ["React Native", "Android Studio", "Xcode"]',
+					code: 'toolsList: ["React Native", "Android Studio", "Xcode"],',
 					explain: 'This app was intended to be very lightweight. I did not use any outside libraries, just the built-in components.'
 				}),
 				codeClass: 'objProp2'
@@ -218,14 +218,14 @@ const projectComponents = [
 		// },
 		// 	{
 		// 		code: codeExplain({
-		// 			code: 'name: "WakeMe"',
+		// 			code: 'name: "WakeMe",',
 		// 			explain: 'WakeMe is where an alarm app meets Snapchat. You can send pics/vids to friends, but they cannot view them until the next morning when their alarm goes off.'
 		// 		}),
 		// 		codeClass: 'objProp2'
 		// 	},
 		// 	{
 		// 		code: codeExplain({
-		// 			code: 'toolsUsed: ["React Native", "Android Studio", "Xcode", "AWS(Lambda, DB, S3)", "Serverless Framework", "OneSignal"]',
+		// 			code: 'toolsUsed: ["React Native", "Android Studio", "Xcode", "AWS(Lambda, DB, S3)", "Serverless Framework", "OneSignal"],',
 		// 			explain: "I used this project to experiment with a serverless architecture using AWS technology and the Serverless Framework to build efficient APIs. OneSignal is the platform I used to send push notifications to devices."
 		// 		}),
 		// 		codeClass: 'objProp2'
@@ -272,21 +272,21 @@ const experienceComponents = [
 		},
 			{
 				code: codeExplain({
-					code: 'company: "Plexuss"',
+					code: 'company: "Plexuss",',
 					explain: <span>My current employer is Plexuss and we're a college recruiting network. Check us out at <a href="https://plexuss.com" target="_blank">plexuss.com</a></span>
 				}),
 				codeClass: 'objProp2'
 			},
 			{
 				code: codeExplain({
-					code: 'role: "Software Engineer"',
+					code: 'role: "Software Engineer",',
 					explain: "Being a fullstack developer at Plexuss means that I am constantly bouncing from React to Node to the AWS console."
 				}),
 				codeClass: 'objProp2'
 			},
 			{
 				code: codeExplain({
-					code: 'when: "2014 - Present"',
+					code: 'when: "2014 - Present",',
 					explain: "It has been a great learning experience in my 3 years here."
 				}),
 				codeClass: 'objProp2'
@@ -326,28 +326,28 @@ const educationComponents = [
 	},
 		{
 			code: codeExplain({
-				code: 'School: "Arizona State University"',
+				code: 'School: "Arizona State University",',
 				explain: "ASU is my alma mater and is the place where I learned that I wanted to be a developer of many facets."
 			}),
 			codeClass: 'objProp2'
 		},
 		{
 			code: codeExplain({
-				code: 'major: "Computer Information Systems"',
+				code: 'major: "Computer Information Systems",',
 				explain: "Though not quite the equivalent of a computer science degree, I learned the basics of programming and the internet."
 			}),
 			codeClass: 'objProp2'
 		},
 		{
 			code: codeExplain({
-				code: 'Graduated: "2014"',
+				code: 'Graduated: "2014",',
 				explain: 'Graduated: "2014"'
 			}),
 			codeClass: 'objProp2'
 		},
 		{
 			code: codeExplain({
-				code: 'didVolunteer: true',
+				code: 'didVolunteer: true,',
 				explain: "I volunteered my time doing various activities including tutoring at Microsoft App-Building events and instructing at the Boys and Girls Club."
 			}),
 			codeClass: 'objProp2'
