9b488e62a7649f4bdffec1db7dd97162b123d9db
Adam Adams <ajadams10@gmail.com>
2018-02-25 13:23:39 -0800

    adding webpack uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require('webpack');
 
 module.exports = {
 
@@ -26,5 +27,9 @@ module.exports = {
             } 
 
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+    ]
 };
